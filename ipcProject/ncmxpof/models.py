1084cede8342cdda8cff207e3f09ceec23314365
marcusxdm <marcuspestana@ic.ufal.br>
2017-11-28 17:03:17 -0300

    Arroz to Tomate POF done

--- ipcProject/ncmxpof/models.py
+++ ipcProject/ncmxpof/models.py
@@ -8,11 +8,11 @@ from django.db import models
 
 class Pof(models.Model):
     cod      = models.BigIntegerField()
-    descript = models.CharField(max_length=200)
+    descript = models.CharField(max_length=500)
 
 class Ncm(models.Model):
     cod      = models.BigIntegerField()
-    descript = models.CharField(max_length=200)
+    descript = models.CharField(max_length=500)
     pof      = models.ForeignKey(Pof, on_delete=models.CASCADE)
 
     def __unicode__(self):
